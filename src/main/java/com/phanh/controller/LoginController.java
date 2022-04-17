9b9084aa97a26db75f8197d7c560f0d6e35b5dc0
your name <phanh2412@gmail.com>
2022-04-17 10:48:36 +0700

    init

---
+++ src/main/java/com/phanh/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.phanh.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginController extends HttpServlet{
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        PrintWriter out = resp.getWriter();
+        out.print("<h1>" + username + "</h1>");
+        out.print("<h1>" + password + "</h1>");
+    }
+}
---
+++ src/main/webapp/view/register.jsp
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <label>Username</label>
+        <input type="text" name="username"><br><br>
+        <label>Password</label>
+        <input type="password" name="password"><br><br>
+        <label>Re-Password</label>
+        <input type="password" name="re_password"><br><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
